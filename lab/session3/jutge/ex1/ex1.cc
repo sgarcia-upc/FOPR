2d18dbed30553f70f692caed15e5f5579ddeb74d
e7989070 <e7989070@aul-1610>
2019-09-23 14:20:22 +0200

    Adding lab sessions

---
+++ lab/session3/jutge/ex1/ex1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a,b;
+
+    cin >> a;
+    cin >> b;
+
+    if (a > b ){
+        cout << a << endl;
+    } else {
+        cout << b << endl;
+    }
+}
